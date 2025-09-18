c84ef909462b91e86e2c1b03c8504b823cd45135
Administrator <Administrator@DESKTOP-FUGQNF4>
2025-09-18 13:29:21 +0100

    changes

--- fs-a-2025-blazor-0918-72746/Program.cs
+++ fs-a-2025-blazor-0918-72746/Program.cs
@@ -1,4 +1,5 @@
 using fs_a_2025_blazor_0918_72746.Components;
+using fs_a_2025_blazor_0918_72746.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,22 +7,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddRazorComponents();
+builder.Services.AddScoped<CarRepository>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
-
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
---
+++ fs-a-2025-blazor-0918-72746/Repositories/CarRepository.cs
@@ -0,0 +1,45 @@
+﻿namespace fs_a_2025_blazor_0918_72746.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using FleetManager.Models;
+
+    public class CarRepository
+    {
+        private readonly List<Car> _cars;
+
+        public CarRepository()
+        {
+            _cars = GenerateMockCars();
+        }
+
+        public List<Car> GetAllCars()
+        {
+            return _cars;
+        }
+
+        private List<Car> GenerateMockCars()
+        {
+            var cars = new List<Car>();
+            var makes = new[] { "Toyota", "Honda", "Ford", "Chevrolet", "BMW" };
+            var models = new[] { "Corolla", "Civic", "Focus", "Cruze", "3 Series" };
+            var colors = new[] { "Red", "Blue", "Black", "White", "Silver" };
+            var rnd = new Random();
+
+            for (int i = 1; i <= 25; i++)
+            {
+                cars.Add(new Car
+                {
+                    Id = i,
+                    Make = makes[rnd.Next(makes.Length)],
+                    Model = models[rnd.Next(models.Length)],
+                    Year = rnd.Next(2015, 2024),
+                    Color = colors[rnd.Next(colors.Length)],
+                    Mileage = rnd.Next(10_000, 150_000)
+                });
+            }
+
+            return cars;
+        }
+    }
+}
